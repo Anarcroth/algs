5f2766004dbccd3243df7606350d4d224d7b72eb
Martin Nestorov <mnestorov@protonmail.com>
2019-02-01 14:02:05 +0200

    Adds task description

--- profit.cpp
+++ profit.cpp
@@ -1,3 +1,13 @@
+// Task profit
+// Several cells are arranged one after the other in a straight line. In `i-th` cell, a given integer `ai` is written (i=1,2, ..., N). I start from the first cell at the left end and move right. I can choose to jump into the next cell, or into the next of the next cell. Every time I entered a cell `i`, I have to pay |ai| dollars, when `ai` is negative, or to receive `ai` dollars, when `ai` is non negative. At most, how many dollars can I earn?
+// Input: Integer values of N, a1, a2, ...., aN,separated by spaces.
+// Output: One integer equals to the wanted profit.
+// Constraints: 0 < N < 100; -100 < ai < 100 for each ai.
+// Example Input
+// 7 2 -1 3 -2 -1 6 -5
+// Output
+// 10
+
 #include <iostream>
 #include <vector>
 
